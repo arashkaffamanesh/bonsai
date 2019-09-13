150e18404039a669bf1d32bd259679f57b11f870
Arash Kaffamanesh <arash.kaffamanesh.ext@dkv-mobility.com>
2019-09-13 21:50:41 +0200

    kubectl download disabled, some hints for linux users

--- 3-deploy-rancher-on-k3s.sh
+++ 3-deploy-rancher-on-k3s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export KUBECONFIG=k3s.yaml
-./kubectl -n kube-system create serviceaccount tiller
-./kubectl create clusterrolebinding tiller --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 curl -LO https://git.io/get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh

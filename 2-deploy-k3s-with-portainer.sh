f6961bc9e2f102df93550d368747a11e63999325
Alexander Giesa <alexander@kubernauts.de>
2020-06-17 18:10:32 +0200

    depoyment process update

--- 2-deploy-k3s-with-portainer.sh
+++ 2-deploy-k3s-with-portainer.sh
@@ -3,42 +3,25 @@ GREEN='\033[0;32m'
 LB='\033[1;34m' # light blue
 NC='\033[0m' # No Color
 
-echo -e "[${LB}Info${NC}] deploy k3s on k3s-master"
-multipass exec k3s-master -- /bin/bash -c "curl -sfL https://get.k3s.io | sh -"
-# Get the IP of the master node
-K3S_NODEIP_MASTER="https://$(multipass info k3s-master | grep "IPv4" | awk -F' ' '{print $2}'):6443"
-# Get the TOKEN from the master node
-K3S_TOKEN="$(multipass exec k3s-master -- /bin/bash -c "sudo cat /var/lib/rancher/k3s/server/node-token")"
-# Deploy k3s on the worker nodes node2,node3,node4
+./2-deploy-k3s.sh
 
-WORKERS=$(echo $(multipass list | grep worker | awk '{print $1}'))
-for WORKER in ${WORKERS}; do echo -e "[${LB}Info${NC}] deploy k3s on ${WORKER}" && multipass exec ${WORKER} -- /bin/bash -c "curl -sfL https://get.k3s.io | K3S_TOKEN=${K3S_TOKEN} K3S_URL=${K3S_NODEIP_MASTER} sh -"; done
-sleep 10
-
-echo "############################################################################"
-echo exporting KUBECONFIG file from master node
-multipass exec k3s-master -- bash -c 'sudo cat /etc/rancher/k3s/k3s.yaml' > k3s.yaml
-sed -i'.back' -e 's/127.0.0.1/k3s-master/g' k3s.yaml
-export KUBECONFIG=`pwd`/k3s.yaml && echo "[${LB}Info${NC}] setting KUBECONFIG=${KUBECONFIG}"
-
-echo -e "[${LB}Info${NC}] tainting master node: k3s-master"
-kubectl taint node k3s-master node-role.kubernetes.io/master=effect:NoSchedule
-
-for WORKER in ${WORKERS}; do kubectl label node ${WORKER} node-role.kubernetes.io/node=  > /dev/null && echo -e "[${LB}Info${NC}] label ${WORKER} with node"; done
+export KUBECONFIG=`pwd`/k3s.yaml && echo -e "[${LB}Info${NC}] setting KUBECONFIG=${KUBECONFIG}"
 
 echo -e "[${LB}Info${NC}] deploy metallb loadbalancer"
-kubectl create -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml
-kubectl create -f metal-lb-layer2-config.yaml
-kubectl rollout status -n metallb-system daemonset speaker
-kubectl get all -A
+kubectl create -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml >/dev/null
+kubectl create -f metal-lb-layer2-config.yaml >/dev/null
+kubectl rollout status -n metallb-system daemonset speaker >/dev/null
 kubectl get nodes
 
 echo "are the nodes ready?"
 echo "if you face problems, please open an issue on github"
-echo "now deploying portainer"
-kubectl create -f addons/portainer/portainer.yaml
-kubectl rollout status -n portainer deployment portainer
+echo -e "[${LB}Info${NC}] deploy portainer"
+kubectl create -f addons/portainer/portainer.yaml  >/dev/null
+kubectl rollout status -n portainer deployment portainer  >/dev/null
 portainer_ip=`kubectl get svc -n portainer | grep portainer | awk 'NR==1{print $4}'`
+
+sleep 5
+
 open http://$portainer_ip:9000
 
 echo -e "[${GREEN}FINISHED${NC}]"
--- cleanup.sh
+++ cleanup.sh
@@ -9,12 +9,45 @@ NODES+=$MASTER
 NODES+=" "
 NODES+=$WORKERS
 
+cleanupAnsw="y"
+read -p  "Do you want to clean your /etc/hosts from multipass entries (y/n)?(default:y) promt with [ENTER]:" input
+cleanupAnsw="${input:-$cleanupAnsw}"
+
+if [ $cleanupAnsw == 'y' ];
+then
+  echo "We need to write the host entries on your local machine to /etc/hosts"
+  echo "Please provide your sudo password:"
+
+  # backup before cleanup
+  cp /etc/hosts hosts.cleanup.backup
+  cp hosts.cleanup.backup hosts
+
+  # seach for existing multipass config
+  exists=$(grep -n "####### multipass hosts start ##########" hosts | awk -F: '{print $1}' | head -1)
+  # check if var is empty
+  if test -z "$exists" 
+  then
+    exists=0
+  fi
+
+  # cut existing config
+  if (("$exists" > "0")) ; then
+    start=$(grep -n "####### multipass hosts start ##########" hosts | awk -F: '{print $1}' | head -1)
+    ((start=start-1))
+    end=$(grep -n "####### multipass hosts end   ##########" hosts | awk -F: '{print $1}' | head -1)
+    sed -i '' ${start},${end}d hosts
+  fi
+
+  # cp cleaned hosts to /etc/hosts
+  cp hosts /etc/hosts
+fi
+
 # Stop then delete nodes
 for NODE in ${NODES}; do multipass stop ${NODE} && multipass delete ${NODE}; done
 # Free discspace
 multipass purge
 
-rm hosts k3s.yaml.back k3s.yaml get_helm.sh etchosts etchosts.unix 2> /dev/null
+rm hosts hosts.backup k3s.yaml.back k3s.yaml get_helm.sh  etchosts.unix 2> /dev/null
 echo -e "[${GREEN}FINISHED${NC}]"
 echo "############################################################################"
 echo -e "[${LB}Info${NC}] Please cleanup the host entries in your /etc/hosts manually"
